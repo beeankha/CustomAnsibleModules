da6ccd4e46e1d0a95af29eea3b2acdf82cd73155
Bianca Henderson <bhenderson@anaconda.com>
2022-06-17 11:40:46 -0400

    Update README

--- library/python_module_sum.py
+++
@@ -1,8 +0,0 @@
-def sum(number_one, number_two):
-    sum = number_one + number_two
-    return sum
-
-number_one = 3
-number_two = 5
-
-print(f"The sum of {number_one} and {number_two} is {sum(number_one, number_two)}")
